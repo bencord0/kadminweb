3f9f7e0ab220930e40a084633797860ddfcfd990
Ben Cordero <bencord0@condi.me>
2021-02-27 18:12:56 +0000

    Add templates to bdist package

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
-import os
+from pathlib import Path
 from setuptools import find_packages, setup
+
+
+def find_templates(directory):
+    return [
+        str(template.relative_to(directory))
+        for template
+        in Path(directory).glob('**/*.html')
+    ]
+
 
 install_requires = [
     'django',
@@ -19,6 +28,10 @@ setup(
     author_email='bencord0@condi.me',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    package_data={
+        "django_kerberos": find_templates('src/django_kerberos'),
+        "kadminweb": find_templates('src/kadminweb'),
+    },
     install_requires=install_requires,
     extras_require=extras_require,
 )
